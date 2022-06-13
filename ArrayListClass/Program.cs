0a3df5afacae88a172408c8c6bf0fda4bd918216
SaberValkyrie <93904326+SaberValkyrie@users.noreply.github.com>
2022-06-13 14:13:38 +0700

    Add project files.

---
+++ ArrayListClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ArrayListClass;
+using System.Collections;
+
+
+namespace ArrayListClass
+{
+    class Program
+    {
+        public static void Main()
+        {
+            ArrayList inv = new ArrayList();
+
+            inv.Add(new Product("A", 5.9, 3));
+            inv.Add(new Product("B", 8.2, 2));
+            inv.Add(new Product("C", 3.5, 4));
+            inv.Add(new Product("D", 1.8, 8));
+            Console.WriteLine("Product List:");
+            foreach (Product i in inv)
+            {
+                Console.WriteLine(" " + i);
+            }
+        }
+    }
+}
